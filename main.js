42d1d7eabc6fe83bb1592ddc66f2072a69d68f9c
Jason Q Bradley <98352141+Bradlj03@users.noreply.github.com>
2022-03-13 14:03:48 -0400

    finished

---
+++ main.js
@@ -0,0 +1,24 @@
+let heading = document.querySelector('h1');
+heading.style.fontSize = '48px';
+
+
+let paragraph = documment.querySelector('p');
+paragraph.style.color = 'lightblue';
+paragraph.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.';
+
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = '0.50'
+
+let item3 = document.querySelector('#item-3');
+item3.innerText = '| say, "HI"';
+
+let img1 = document.querySelector('#img-1');
+img.src = 'https://resize.hswstatic.com/w_1024/gif/t-rex.jpg';
+img.style.height = '300px';
+
+let img2 = document.querySelector('#img-2');
+img2.style.height = '300px';
+
+let item16 = document.querySelector('#item-16');
+item16.className = item3;
+item16.innerText = "Won't get fooled again.";
